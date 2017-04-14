7fc7cb42797a79460fcb7fbe95837f9bacf9511e
Andy B <andybarnov@gmail.com>
2017-04-14 21:47:34 +0300

    rubocop ok:

--- rubotnik/postback_dispatch.rb
+++ rubotnik/postback_dispatch.rb
@@ -1,10 +1,12 @@
 module Rubotnik
-  # POSTBACKS
+  # Routing for postbacks
   class PostbackDispatch
     include Commands
 
     def initialize(postback)
       @postback = postback
+      p @postback.class
+      p @postback
       @user = UserStore.instance.find_or_create_user(@postback.sender['id'])
     end
 
@@ -16,29 +18,37 @@ module Rubotnik
     private
 
     def bind(regex_string, to: nil, start_thread: {})
-      if @postback.payload == regex_string.upcase
-        @user.reset_command # Stop any current interaction
-        @user.answers = {} # Reset whatever you stored in the user
-        @matched = true
-        puts "Matched #{regex_string} to #{to.nil? ? 'block' : to}"
-        if block_given?
-          yield
-          return
-        end
-        if start_thread.empty?
-          execute(to)
-          puts "Command #{to} is executed for user #{@user.id}"
-          @user.reset_command
-          puts "Command is reset for user #{@user.id}"
-        else
-          say(start_thread[:message], quick_replies: start_thread[:quick_replies])
-          @user.set_command(to)
-          puts "Command #{to} is set for user #{@user.id}"
-        end
+      return unless @postback.payload == regex_string.upcase
+      clear_user_state
+      @matched = true
+      puts "Matched #{regex_string} to #{to.nil? ? 'block' : to}"
+      if block_given?
+        yield
+        return
+      end
+      handle_commands(to, start_thread)
+    end
+
+    def handle_commands(to, start_thread)
+      if start_thread.empty?
+        execute(to)
+        puts "Command #{to} is executed for user #{@user.id}"
+        @user.reset_command
+        puts "Command is reset for user #{@user.id}"
+      else
+        say(start_thread[:message], quick_replies: start_thread[:quick_replies])
+        @user.set_command(to)
+        puts "Command #{to} is set for user #{@user.id}"
       end
     end
+
+    def clear_user_state
+      @user.reset_command # Stop any current interaction
+      @user.answers = {} # Reset whatever you stored in the user
+    end
+
     def execute(command)
       method(command).call
     end
   end
-end 
+end

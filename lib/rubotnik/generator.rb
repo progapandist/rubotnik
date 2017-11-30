ead1f9bcd7c565708ae97d113b8a5a0b4c5157bf
Andy B <andybarnov@gmail.com>
2017-11-30 11:08:04 +0100

    generate .env and Procfile

--- lib/rubotnik/generator.rb
+++ lib/rubotnik/generator.rb
@@ -12,6 +12,9 @@ class Rubotnik::Generator < Thor::Group
 
   def create_file_structure
    copy_file 'config.ru', 'config.ru'
+   copy_file 'Gemfile', 'Gemfile'
+   copy_file '.env', '.env'
+   copy_file 'Procfile', 'Procfile'
    copy_file 'commands.rb', 'bot/commands/commands.rb'
    copy_file 'main.rb', 'bot/main.rb'
   end

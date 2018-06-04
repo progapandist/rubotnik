d169b72c674273ee2385aa9fb57d5c20e5c0d4ef
Jan Bajena <bajena3@gmail.com>
2018-06-04 22:48:17 +0200

    Change return to if/else

--- lib/rubotnik.rb
+++ lib/rubotnik.rb
@@ -23,15 +23,17 @@ module Rubotnik
   end
 
   def self.route(event, &block)
-    return Bot.on(event, &block) unless [:message, :postback].include?(event)
-
-    Bot.on event do |e|
-      case e
-      when Facebook::Messenger::Incoming::Message
-        Rubotnik::MessageDispatch.new(e).route(&block)
-      when Facebook::Messenger::Incoming::Postback
-        Rubotnik::PostbackDispatch.new(e).route(&block)
+    if [:message, :postback].include?(event)
+      Bot.on event do |e|
+        case e
+        when Facebook::Messenger::Incoming::Message
+          Rubotnik::MessageDispatch.new(e).route(&block)
+        when Facebook::Messenger::Incoming::Postback
+          Rubotnik::PostbackDispatch.new(e).route(&block)
+        end
       end
+    else
+      Bot.on(event, &block)
     end
   end
 
